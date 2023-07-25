9e72df19c7642ec34e7ebfb04e9bb015811e0430
Robert Huselius <robert@huseli.us>
2023-07-25 02:03:32 +0200

    Bumpo version + new APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "us.huseli.umpc"
         minSdk = 26
         targetSdk = 33
-        versionCode = 5
-        versionName = "1.0.0-beta.5"
+        versionCode = 6
+        versionName = "1.0.0-beta.6"
         // multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
