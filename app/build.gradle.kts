a5460d97931e60fcdf24993de3c91753f0540839
Robert Huselius <robert@huseli.us>
2023-08-30 00:10:28 +0200

    New APK build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "us.huseli.umpc"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.0.0-beta.8"
+        versionCode = 9
+        versionName = "1.0.0-beta.9"
         // multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
